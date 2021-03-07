9d378ca8ae55425f6d52653ccb74c43549d7d483
oliver hodge <oliverhodge2@ufl.edu>
2021-03-07 16:04:49 +0100

    changed response

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 exports.handler = async (event) => {
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hello oliver from Lambda and Github!'),
+        body: JSON.stringify('Hello yall from Lambda and Github!'),
     };
     return response;
 };
